2c85fd354bd6db2c7129b4f0d68f3cbc94f191cb
wordaddict_ <madeyinka@terragonltd.com>
2018-07-29 19:31:25 +0000

    base app for skype

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './Sidebar'
+import Main from './Main'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
