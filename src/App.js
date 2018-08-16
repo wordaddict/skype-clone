d776163095ef5b00de8cc96f82d7c0d9c1eb5d61
wordaddict_ <madeyinka@terragonltd.com>
2018-08-16 12:12:44 +0100

    side bar done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar'
 import Main from './Main'
+import { contacts } from './static-data';
+import _ from 'lodash';
 
 const App = () => {
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar contacts={_.values(contacts)}/>
       <Main />
     </div>
   );
