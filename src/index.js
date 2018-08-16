4de16e92a5cc0fb6a5af39cfb3c3f0a6f526aad4
wordaddict_ <madeyinka@terragonltd.com>
2018-08-16 10:55:35 +0100

    added a store, a reducer and a dummy function to fetch data

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = () => {
+    fancyLog();
+    return ReactDOM.render(<App />, document.getElementById('root'));
+};
+render();
+store.subscribe(render);
 registerServiceWorker();
+
+
+function fancyLog() {
+    console.log("%c Rendered with 👉 👉 👇 ", "background: purple;color: #FFF");
+    console.log(store.getState());
+}
